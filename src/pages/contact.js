a6cc978861274710b59e0dffb554c28c3cb816e9
John Law <johnlaw.po@gmail.com>
2019-09-14 08:20:53 +0800

    Modify link

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,6 +5,7 @@ import Header from "../components/header"
 export default () => (
 	<div style={{ color: `teal` }}>
 	<Link to="/">Home...</Link>
+	<Link to="/about/">About...</Link>
 	<Header headerText="Contact" />
 	<p>Send us a message!</p>
 	</div>
